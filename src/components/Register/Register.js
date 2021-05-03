04f616b2d16316eef0850b1ce9b3cd71fa0b484e
Ksenia <you@example.com>
2021-05-03 21:59:41 +0200

    fix

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,7 +4,7 @@ function Register (props) {
     return (
 <AuthForm title="Добро пожаловать!" buttonText="Зарегистрироваться" linkText="Уже зарегистрированы?" logText="Войти" onLog="/sign-in">
 <label className="auth-form__label" htmlFor="name">Имя</label>
-         <input type="text" className="auth-form__input-name auth-form__input" name="name" id="name"/>
+         <input type="text" className="auth-form__input-name auth-form__input" name="name" id="name" required/>
          <span className="auth-form__error">Что-то пошло не так</span>
 </AuthForm>
     )
